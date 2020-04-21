6ba2a8eb3349a94eb723f678856a8b6e10e3435a
Alex Marshall <alex.k.marshall83@gmail.com>
2020-04-21 16:07:11 +0100

    Show error on invalid input

--- script.js
+++ script.js
@@ -1,65 +1,114 @@
 const form = document.querySelector("form");
 form.setAttribute("novalidate", true);
+const emailField = document.querySelector("#email");
+const emailErrorText = document.querySelector("#error-for-email");
+const emailIcon = document.querySelector("#icon-for-email");
 
-const hasError = (field) => {
-  if (field.type === "button") return;
-  if (field.validity.valid) return;
-  return "Please provide a valid email";
+const showElement = (element) => {
+  element.style.display = "block";
+  element.style.visibility = "visible";
 };
 
-const showError = (field, error) => {
-  field.classList.add("error");
-  const id = field.id || field.name;
-  if (!id) return;
-
-  const errorId = `error-for-${id}`;
-
-  let message = field.form.querySelector(`.error-message#${errorId}`);
-  if (!message) {
-    message = document.createElement("div");
-    message.className = "error-message";
-    message.id = errorId;
-    field.parentNode.insertBefore(message, field.nextSibling);
-  }
-
-  field.setAttribute("aria-describedby", errorId);
-
-  message.innerHTML = error;
-
-  message.style.display = "block";
-  message.style.visibility = "visible";
+const hideElement = (element) => {
+  element.style.display = "none";
+  element.style.visibility = "hidden";
 };
 
-const removeError = (field) => {
-  field.classList.remove("error");
+const inputIsValid = (field) => {
+  return field.validity.valid;
+};
 
-  field.removeAttribute("aria-describedby");
-
-  const id = field.id || field.name;
-  if (!id) return;
-
-  const message = field.form.querySelector(`.error-message#error-for-${id}`);
-  if (!message) return;
-
-  message.innerHTML = "";
-  message.style.display = "none";
-  message.style.visibility = "hidden";
+const showError = () => {
+  emailField.classList.add("error");
+  [emailErrorText, emailIcon].forEach((element) => {
+    showElement(element);
+  });
+  emailField.setAttribute("aria-describedby", "error-for-email");
 };
 
 document.addEventListener(
   "blur",
   (event) => {
-    const error = hasError(event.target);
+    if (event.target.id !== "email") return;
 
-    if (error) {
-      showError(event.target, error);
+    const valid = inputIsValid(event.target);
+
+    if (!valid) {
+      showError();
       return;
     }
-
-    removeError(event.target);
   },
   true
 );
 
-const emailField = document.querySelector("input");
-const validityState = emailField.validity;
+// emailField.addEventListener(blur, (event) => {
+//   console.log("in listener", event.target);
+//   const isValid = inputIsValid(event.target);
+//   if (!valid) {
+//     showError();
+//     return;
+//   }
+// });
+
+// const hasError = (field) => {
+//   if (field.type === "button") return;
+//   if (field.validity.valid) return;
+//   return "Please provide a valid email";
+// };
+
+// const showErrorOld = (field, error) => {
+//   field.classList.add("error");
+//   const id = field.id || field.name;
+//   if (!id) return;
+
+//   const errorId = `error-for-${id}`;
+
+//   let message = field.form.querySelector(`.error-message#${errorId}`);
+//   if (!message) {
+//     message = document.createElement("div");
+//     message.className = "error-message";
+//     message.id = errorId;
+//     field.parentNode.insertBefore(message, field.nextSibling);
+//   }
+
+//   field.setAttribute("aria-describedby", errorId);
+
+//   message.innerHTML = error;
+
+//   message.style.display = "block";
+//   message.style.visibility = "visible";
+// };
+
+// const removeError = (field) => {
+//   field.classList.remove("error");
+
+//   field.removeAttribute("aria-describedby");
+
+//   const id = field.id || field.name;
+//   if (!id) return;
+
+//   const message = field.form.querySelector(`.error-message#error-for-${id}`);
+//   if (!message) return;
+
+//   message.innerHTML = "";
+//   message.style.display = "none";
+//   message.style.visibility = "hidden";
+// };
+
+// document.addEventListener(
+//   "blur",
+//   (event) => {
+//     const error = hasError(event.target);
+
+//     if (error) {
+//       showErrorOld(event.target, error);
+//       return;
+//     }
+
+//     removeError(event.target);
+//   },
+//   true
+// );
+
+// const emailField = document.querySelector("input");
+// const validityState = emailField.validity;
