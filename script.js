23ea85c84d65e3aed015d938327859d5b7726b44
Alex Marshall <alex.k.marshall83@gmail.com>
2020-04-21 16:12:26 +0100

    Hide error messages when input is correct

--- script.js
+++ script.js
@@ -26,6 +26,14 @@ const showError = () => {
   emailField.setAttribute("aria-describedby", "error-for-email");
 };
 
+const removeError = () => {
+  emailField.classList.remove("error");
+  [emailErrorText, emailIcon].forEach((element) => {
+    hideElement(element);
+  });
+  emailField.removeAttribute("aria-describedby");
+};
+
 document.addEventListener(
   "blur",
   (event) => {
@@ -37,78 +45,8 @@ document.addEventListener(
       showError();
       return;
     }
+
+    removeError();
   },
   true
 );
-
-// emailField.addEventListener(blur, (event) => {
-//   console.log("in listener", event.target);
-//   const isValid = inputIsValid(event.target);
-//   if (!valid) {
-//     showError();
-//     return;
-//   }
-// });
-
-// const hasError = (field) => {
-//   if (field.type === "button") return;
-//   if (field.validity.valid) return;
-//   return "Please provide a valid email";
-// };
-
-// const showErrorOld = (field, error) => {
-//   field.classList.add("error");
-//   const id = field.id || field.name;
-//   if (!id) return;
-
-//   const errorId = `error-for-${id}`;
-
-//   let message = field.form.querySelector(`.error-message#${errorId}`);
-//   if (!message) {
-//     message = document.createElement("div");
-//     message.className = "error-message";
-//     message.id = errorId;
-//     field.parentNode.insertBefore(message, field.nextSibling);
-//   }
-
-//   field.setAttribute("aria-describedby", errorId);
-
-//   message.innerHTML = error;
-
-//   message.style.display = "block";
-//   message.style.visibility = "visible";
-// };
-
-// const removeError = (field) => {
-//   field.classList.remove("error");
-
-//   field.removeAttribute("aria-describedby");
-
-//   const id = field.id || field.name;
-//   if (!id) return;
-
-//   const message = field.form.querySelector(`.error-message#error-for-${id}`);
-//   if (!message) return;
-
-//   message.innerHTML = "";
-//   message.style.display = "none";
-//   message.style.visibility = "hidden";
-// };
-
-// document.addEventListener(
-//   "blur",
-//   (event) => {
-//     const error = hasError(event.target);
-
-//     if (error) {
-//       showErrorOld(event.target, error);
-//       return;
-//     }
-
-//     removeError(event.target);
-//   },
-//   true
-// );
-
-// const emailField = document.querySelector("input");
-// const validityState = emailField.validity;
